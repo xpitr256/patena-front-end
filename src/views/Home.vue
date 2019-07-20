c49ba11fd8610ce4bdeecfb74a4e06050e6e6954
Nicolas Coco <nicolascoco85@gmail.com>
2019-07-20 19:25:36 -0300

    ADD DESCRIPTION WELCOME

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,20 @@
   <div class="home">
     <img alt="Patena logo" src="../assets/logo.png" />
     <HomeBody />
+    <Textbox />
   </div>
 </template>
 
 <script>
 import HomeBody from "@/components/HomeBody.vue";
+import Textbox from "@/components/Textbox.vue";
 
 export default {
   name: "home",
   components: {
-    HomeBody
+    HomeBody,
+    // eslint-disable-next-line vue/no-unused-components
+    Textbox
   }
 };
 </script>
