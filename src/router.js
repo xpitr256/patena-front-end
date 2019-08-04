c0f804d275a22cc629701220cb15e37c3749cb21
Nicolas Coco <nicolascoco85@gmail.com>
2019-08-04 19:45:47 -0300

    Se crea la pantalla analizar sin drag&drop para el archivo

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Contact from "./views/Contact";
 import ViewResult from "./views/ViewResult";
+import Analyze from "./views/Analize";
 
 Vue.use(Router);
 
@@ -33,6 +34,11 @@ export default new Router({
       path: "/results",
       name: "results",
       component:ViewResult
+    },
+    {
+      path: "/analyze",
+      name: "Analyze",
+      component:Analyze
     }
   ]
 });
---
+++ src/views/Analize.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container wrapper">
+        <AnalyzeBody/>
+    </div>
+</template>
+
+<script>
+    import AnalyzeBody from "../components/AnalyzeBody";
+    export default {
+        components: {AnalyzeBody}
+    }
+</script>
+
+<style scoped>
+
+</style>
