7c2ac570cce5f996f20d5c95abfe0dbd3ae1fd0c
Pablo Rodriguez Massuh <pablomassuh@gmail.com>
2019-11-15 21:07:54 -0300

    Added more test cases related to fasta files.

--- src/services/FastaService.js
+++ src/services/FastaService.js
@@ -1,14 +1,56 @@
-export async function isValidFastaFile(fastaFile) {
-  return true; // TODO do it
+const aminoAcids = [
+  "A",
+  "R",
+  "N",
+  "D",
+  "B",
+  "C",
+  "E",
+  "Q",
+  "Z",
+  "G",
+  "H",
+  "I",
+  "L",
+  "K",
+  "M",
+  "F",
+  "P",
+  "S",
+  "T",
+  "W",
+  "Y",
+  "V"
+];
+
+/**
+ * Returns true | false according to fasta validations
+ */
+export function isValidFasta(fastaContent) {
+  const lines = fastaContent.split("\n");
+  const linesWithoutComments = lines.filter(line => !line.startsWith(">"));
+  const allContent = linesWithoutComments.join("").trim();
+  if (!allContent) {
+    return false;
+  }
+  for (const aminoAcid of allContent) {
+    if (!aminoAcids.includes(aminoAcid)) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-export async function getSequenceLengthFrom(fastaFile) {
+/**
+ * It tries to open the fasta file and return the whole content as a string.
+ */
+export async function getFastaFileContent(fastaFile) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsText(fastaFile, "UTF-8");
     reader.onload = function(evt) {
-      const lines = evt.target.result.split("\n");
-      resolve(lines);
+      resolve(evt.target.result);
     };
     reader.onerror = function(evt) {
       reject(new Error("Error reading file"));
@@ -16,4 +58,6 @@ export async function getSequenceLengthFrom(fastaFile) {
   });
 }
 
-export default { isValidFastaFile, getSequenceLengthFrom };
+export async function getSequenceLengthFrom(fastaContent) {}
+
+export default { isValidFasta, getFastaFileContent, getSequenceLengthFrom };
--- test/services/FastaServiceTest.js
+++ test/services/FastaServiceTest.js
@@ -1,11 +1,65 @@
 let assert = require("assert");
 import fastaService from "../../src/services/FastaService.js";
 
+const fs = require("fs");
+
 describe("FastaService Test ", function() {
   describe("Validate fasta file ", function() {
     it("should return true for valid file", function() {
-      //TODO
-      assert(fastaService.isValidFastaFile());
+      try {
+        const data = fs.readFileSync(
+          "./test/services/validFasta.fasta",
+          "utf8"
+        );
+        assert(fastaService.isValidFasta(data));
+      } catch (err) {
+        throw err;
+      }
+    });
+
+    it("should return false for wrong amino acid", function() {
+      try {
+        const data = fs.readFileSync(
+          "./test/services/invalidAminoAcidCharacter.fasta",
+          "utf8"
+        );
+        assert.strictEqual(fastaService.isValidFasta(data), false);
+      } catch (err) {
+        throw err;
+      }
+    });
+
+    it("should return false for empty files", function() {
+      try {
+        const data = fs.readFileSync("./test/services/emptyFile.fasta", "utf8");
+        assert.strictEqual(fastaService.isValidFasta(data), false);
+      } catch (err) {
+        throw err;
+      }
+    });
+
+    it("should return false for files with no amino acid but comments", function() {
+      try {
+        const data = fs.readFileSync(
+          "./test/services/noAminoAcid.fasta",
+          "utf8"
+        );
+        assert.strictEqual(fastaService.isValidFasta(data), false);
+      } catch (err) {
+        throw err;
+      }
+    });
+
+    it("should return false for files with spaces between amino acids", function() {
+      try {
+        const data = fs.readFileSync(
+          "./test/services/spacesBetweenAminoAcid.fasta",
+          "utf8"
+        );
+        assert.strictEqual(fastaService.isValidFasta(data), false);
+      } catch (err) {
+        throw err;
+      }
     });
   });
 });
