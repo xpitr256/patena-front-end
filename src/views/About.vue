10672b4e8faebb36a507a0932debc5ca1db73a9b
nicolas coco <nicolascoco85@yahoo.com.ar>
2021-10-23 21:12:37 -0300

    refactoring about us

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,41 +5,25 @@
       <p class="lead">{{ $t("views.about.description") }}</p>
     </div>
     <div class="row">
-      <div class="col-4">
-        <div class="card">
-          <img src="../assets/img/person.jpg" class="card-img-top" alt="Lucio Aliperti" />
-          <div class="card-body">
-            <h5 class="card-title">Lucio Aliperti</h5>
-            <p class="card-text">
-              Some quick background about Lucio, his studies, current work, hobbies, etc.
-            </p>
-            <a href="#" class="btn btn-primary">Contact me</a>
-          </div>
-        </div>
+      <div class="col-6">
+        <ul class="list-group">
+          <li class="list-group-item list-group-item-secondary">Ignacio E. Sánchez</li>
+          <li class="list-group-item list-group-item-light">Investigador que co-organiza cursos en Biología
+            sintética y es "Profesor Adjunto" en la UADE, para la materia Biología de Sistemas en la Licenciatura en bioinformática. También fue "Jefe de trabajos prácticos" en las materias "Química Biológica I" y "Química Biológica IIB" del Departamento de Química Biológica (FCEyN, Universidad de Buenos Aires)
+          </li>
+          <br>
+          <li class="list-group-item list-group-item-secondary">Diego U. Ferreiro</li>
+          <li class="list-group-item list-group-item-light">Profesor Adjunto en el Departamento de Química Biológica (FCEyN, Universidad de Buenos Aires), en la materia "Bioquímica Avanzada". También fue "Jefe de trabajos prácticos" en el Departamento de Fisiología y Biología Molecular y Celular (FCEyN, Universidad de Buenos Aires).</li>
+          <br>
+          <li class="list-group-item list-group-item-secondary">Ignacio Eguinoa</li>
+          <li class="list-group-item list-group-item-light">Analista programador egresado de la Universidad Nacional de
+            La Plata. Uno de los principales colabores del proyecto Patena con su tesis titulada "Implementación de una
+            herramienta bioinformática para el
+            diseño de secuencias linker" </li>
+        </ul>
       </div>
-      <div class="col-4">
-        <div class="card">
-          <img src="../assets/img/person.jpg" class="card-img-top" alt="Lucio Aliperti" />
-          <div class="card-body">
-            <h5 class="card-title">Lucio Aliperti</h5>
-            <p class="card-text">
-              Some quick background about Lucio, his studies, current work, hobbies, etc.
-            </p>
-            <a href="#" class="btn btn-primary">Contact me</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-4">
-        <div class="card">
-          <img src="../assets/img/person.jpg" class="card-img-top" alt="Lucio Aliperti" />
-          <div class="card-body">
-            <h5 class="card-title">Lucio Aliperti</h5>
-            <p class="card-text">
-              Some quick background about Lucio, his studies, current work, hobbies, etc.
-            </p>
-            <a href="#" class="btn btn-primary">Contact me</a>
-          </div>
-        </div>
+      <div class="col-6">
+        <img src="./../assets/EmbeddedImage.png" />
       </div>
     </div>
   </div>
@@ -50,3 +34,12 @@ export default {
   name: "about"
 };
 </script>
+
+<style scoped>
+  #over img {
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+  }
+</style>
+
