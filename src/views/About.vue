411ad48b746822d3fbf6324e21d0591f90f38dbf
nicolas coco <nicolascoco85@yahoo.com.ar>
2021-10-26 14:48:17 -0300

    Translated texts are added

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,25 +7,22 @@
     <div class="row">
       <div class="col-4">
         <ul class="list-group">
-          <li class="list-group-item list-group-item-secondary"><h5>Ignacio E. Sánchez</h5></li>
-          <li class="list-group-item list-group-item-light">Investigador que co-organiza cursos en Biología
-            sintética y es "Profesor Adjunto" en la UADE, para la materia Biología de Sistemas en la Licenciatura en bioinformática. También fue "Jefe de trabajos prácticos" en las materias "Química Biológica I" y "Química Biológica IIB" del Departamento de Química Biológica (FCEyN, Universidad de Buenos Aires)
+          <li class="list-group-item list-group-item-secondary"><h5>{{ $t("views.about.personOneName") }}</h5></li>
+          <li class="list-group-item list-group-item-light size-text">{{ $t("views.about.personOneDescription") }}
           </li>
         </ul>
       </div>
         <div class="col-4">
             <ul class="list-group">
-          <li class="list-group-item list-group-item-secondary"><h5>Diego U. Ferreiro</h5></li>
-          <li class="list-group-item list-group-item-light">Profesor Adjunto en el Departamento de Química Biológica (FCEyN, Universidad de Buenos Aires), en la materia "Bioquímica Avanzada". También fue "Jefe de trabajos prácticos" en el Departamento de Fisiología y Biología Molecular y Celular (FCEyN, Universidad de Buenos Aires).</li>
+          <li class="list-group-item list-group-item-secondary"><h5>{{ $t("views.about.personTwoName") }}</h5></li>
+          <li class="list-group-item list-group-item-light size-text"> {{ $t("views.about.personTwoDescription") }}</li>
             </ul>
         </div>
         <div class="col-4">
             <ul class="list-group">
-          <li class="list-group-item list-group-item-secondary"><h5>Ignacio Eguinoa</h5></li>
-          <li class="list-group-item list-group-item-light">Analista programador egresado de la Universidad Nacional de
-            La Plata. Uno de los principales colabores del proyecto Patena con su tesis titulada "Implementación de una
-            herramienta bioinformática para el
-            diseño de secuencias linker" </li>
+          <li class="list-group-item list-group-item-secondary"><h5>{{ $t("views.about.personThreeName") }}</h5></li>
+          <li class="list-group-item list-group-item-light size-text">{{ $t("views.about.personThreeDescription") }}
+          </li>
         </ul>
 
       </div>
@@ -40,18 +37,8 @@ export default {
 </script>
 
 <style scoped>
-    #centrador{
-        position: relative;
-
-    }
-
-    #imagen{
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        margin: auto;
+    size-text{
+        font-size:1.25vw
     }
 </style>
 
